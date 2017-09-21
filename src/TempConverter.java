92885e868481777e72889ba6215a320dfe0fb6db
MadsEgelund <mads324s@edu.easj.dk>
2017-09-21 14:31:05 +0200

    Initial commit

---
+++ src/TempConverter.java
@@ -0,0 +1,16 @@
+public class TempConverter {
+
+
+        public static void main(String[] args) {
+
+            double celcius = 0; // deklarerer variablen
+            double fahrenheit = 0; // deklarerer variablen
+
+            fahrenheit = 104;
+            celcius = (5.0/9)*(fahrenheit - 32);
+            System.out.println(fahrenheit + " fahrenheit er det samme som " + celcius + " celcius");
+
+        }
+    }
+
+
